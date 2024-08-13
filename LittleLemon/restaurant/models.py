c734e6884de6f472b3a944f3a979f283100b8050
danieldmascaro <danieldmascaro1@gmail.com>
2024-08-13 12:31:47 -0400

    seguridad

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -6,9 +6,16 @@ class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField()
     booking_date = models.DateField(auto_now=False, auto_now_add=False)
+    def __str__(self):
+        return self.name
+    
 
-class Menu(models.Model):
+
+class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField()
+    inventory = models.SmallIntegerField()
+    def __str__(self):
+        return f"{self.title} : {self.price}"
+    
 
--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,29 +1,26 @@
 from django.shortcuts import render
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
-from .serializers import MenuSerializer, BookingSerializer
+from .serializers import MenuItemSerializer, BookingSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .models import *
 from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 
 class MenuView(ListCreateAPIView):
-    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
 
 class SingleMenuView(RetrieveUpdateDestroyAPIView):
-    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
